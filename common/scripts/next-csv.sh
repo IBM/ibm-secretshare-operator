08bd7195877658fd42e607f5322c8313a6fbfd98
Alice <zhuoxi.ibm@gmail.com>
2021-11-05 11:35:36 -0400

    bump version to 1.11.0

--- common/scripts/next-csv.sh
+++ common/scripts/next-csv.sh
@@ -36,13 +36,13 @@ CSV_PATH=bundle/manifests
 
 # Update New CSV
 # replace old CSV value with new one
-sed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
+gsed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
 TIME_STAMP="$(date '+%Y-%m-%dT%H:%M:%S'Z)"
-sed -i "s/2[0-9]*-[0-9]*-[0-9]*T[0-9]*:[0-9]*:[0-9]*Z/$TIME_STAMP/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
+gsed -i "s/2[0-9]*-[0-9]*-[0-9]*T[0-9]*:[0-9]*:[0-9]*Z/$TIME_STAMP/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
 echo "Updated New file with new CSV version"
-sed -i "s/$PREVIOUS_DEV_CSV/$CURRENT_DEV_CSV/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
+gsed -i "s/$PREVIOUS_DEV_CSV/$CURRENT_DEV_CSV/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
 echo "Updated the replaces version line"
 
 #Update version.go to new dev version
-sed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/" version/version.go
+gsed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/" version/version.go
 echo "Updated the version.go with new version"
