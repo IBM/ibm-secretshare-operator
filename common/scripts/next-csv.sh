c41ed0e586af4db1108ada97f3842cd44ed3490e
YCShen1010 <yuchen.shen@mail.utoronto.ca>
2022-09-09 12:40:55 -0700

    update bump up script for secretshare
    Signed-off-by: YCShen1010 <yuchen.shen@mail.utoronto.ca>

--- common/scripts/next-csv.sh
+++ common/scripts/next-csv.sh
@@ -36,13 +36,32 @@ CSV_PATH=bundle/manifests
 
 # Update New CSV
 # replace old CSV value with new one
-gsed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
-TIME_STAMP="$(date '+%Y-%m-%dT%H:%M:%S'Z)"
-gsed -i "s/2[0-9]*-[0-9]*-[0-9]*T[0-9]*:[0-9]*:[0-9]*Z/$TIME_STAMP/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
-echo "Updated New file with new CSV version"
-gsed -i "s/$PREVIOUS_DEV_CSV/$CURRENT_DEV_CSV/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
-echo "Updated the replaces version line"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	# Linux OS
+	sed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
+	TIME_STAMP="$(date '+%Y-%m-%dT%H:%M:%S'Z)"
+	sed -i "s/2[0-9]*-[0-9]*-[0-9]*T[0-9]*:[0-9]*:[0-9]*Z/$TIME_STAMP/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
+	echo "Updated New file with new CSV version"
+	sed -i "s/$PREVIOUS_DEV_CSV/$CURRENT_DEV_CSV/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
+	echo "Updated the replaces version line"
 
-#Update version.go to new dev version
-gsed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/" version/version.go
-echo "Updated the version.go with new version"
+	#Update version.go to new dev version
+	sed -i "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/" version/version.go
+	echo "Updated the version.go with new version"
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+	sed -i ""  "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
+	TIME_STAMP="$(date '+%Y-%m-%dT%H:%M:%S'Z)"
+	sed -i "" "s/2[0-9]*-[0-9]*-[0-9]*T[0-9]*:[0-9]*:[0-9]*Z/$TIME_STAMP/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
+	echo "Updated New file with new CSV version"
+	sed -i "" "s/$PREVIOUS_DEV_CSV/$CURRENT_DEV_CSV/g" $CSV_PATH/ibm-secretshare-operator.clusterserviceversion.yaml
+	echo "Updated the replaces version line"
+
+	#Update version.go to new dev version
+	sed -i "" "s/$CURRENT_DEV_CSV/$NEW_DEV_CSV/" version/version.go
+	echo "Updated the version.go with new version"
+	
+else
+	echo "Not support on other operating system"
+fi
